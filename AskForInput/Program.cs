c42b4a690d01051d279d87d249eb8084598e9c99
Fabio Ribeiro <fabioribeiro.info@gmail.com>
2024-02-21 21:43:48 +0000

    Add string for int, string for float and final result

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,17 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float result = 0;
+
+            Console.WriteLine("Insert a Integer:\n");
+            string str1 = Console.ReadLine();
+            int i = int.Parse(str1);
+            Console.WriteLine("Insert a Real number:\n");
+            string str2 = Console.ReadLine();
+            float w = float.Parse(str2);
+
+            result = i + w;
+            Console.WriteLine(result);
         }
     }
 }
